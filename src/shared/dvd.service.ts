941640f865c8bff1ad49a369506749ae955da97a
mochtioui <mohamed.chtioui@esprit.tn>
2020-12-07 19:51:02 +0100

    4 commit

---
+++ src/shared/dvd.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@angular/core";
+import {FormGroup} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Observable} from "rxjs";
+
+@Injectable(
+  {
+    providedIn: 'root'
+  }
+)
+export class DvdService {
+  form: FormGroup;
+  constructor(private http: HttpClient,private router: Router,private route: ActivatedRoute) {
+  }
+  addDvd(data:any):Observable<any>{
+    return this.http.post('/api/dvd' ,data);
+  }
+}
