e7081f9fbe4cc312c1d760c345a38333bea8b048
mochtioui <mohamed.chtioui@esprit.tn>
2020-12-09 15:43:39 +0100

    Update delete-dvd.component.ts

--- src/app/delete-dvd/delete-dvd.component.ts
+++ src/app/delete-dvd/delete-dvd.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {DvdService} from "../../shared/dvd.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-delete-dvd',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteDvdComponent implements OnInit {
 
-  constructor() { }
+  id;
+  constructor(private dvdService:DvdService,private route: ActivatedRoute, private router: Router,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.dvdService.deleteDvd(this.id).subscribe(()=>{
+      this.router.navigate(['/all']);
+      console.log("deleted");
+    });
   }
 
 }
